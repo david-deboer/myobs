be437309911881c5c9d33e5843cd2ad0df09d7b8
David DeBoer <ddeboer@berkeley.ed>
2020-12-20 14:16:34 -0800

    make satpos script

---
+++ scripts/satpos_script.py
@@ -0,0 +1,8 @@
+from myobs import satcensus
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('tlefile', help="Name of tle file for which to generate satpos script.")
+args = ap.parse_args()
+
+satcensus.satpos_script(args.tlefile)
