1854626512e24ca8dc4750306be2f07fc7b11e77
Dave DeBoer <ddeboer@berkeley.edu>
2021-12-07 13:43:04 -0800

    include git add

--- scripts/beast_tle_update.sh
+++ scripts/beast_tle_update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ~/myobs
+git add tle
 CURRENTDATE=`date +"%Y-%m-%d %T"`
-GCOMMSG='git commit "TLE update '$CURRENTDATE'"'
+GCOMMSG='git commit -m "TLE update '$CURRENTDATE'"'
 eval $GCOMMSG
 git push origin main
